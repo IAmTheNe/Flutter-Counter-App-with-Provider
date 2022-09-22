c20afef598b9ae7dd3e133b86aa13a7d507cd967
Trần Nguyên Thế <nguyenthe.tnt@gmail.com>
2022-09-22 15:20:42 +0700

    First commit

---
+++ android/app/src/main/java/com/iamthene/ex_counter/counter_with_provider/MainActivity.java
@@ -0,0 +1,6 @@
+package com.iamthene.ex_counter.counter_with_provider;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import './providers/counter_provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => CounterProvider(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Counter App'),
+      ),
+      body: const Center(),
+    );
+  }
+}
